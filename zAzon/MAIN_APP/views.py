161fe9cf90668b8f8ab885121c762e4d1dcc5613
Ktwru <spacewarlushd@gmail.com>
2019-08-25 16:00:20 +0300

    sessions
    main_page visits count

--- zAzon/MAIN_APP/views.py
+++ zAzon/MAIN_APP/views.py
@@ -6,7 +6,11 @@ def main_page(request):
     user_count = User.objects.count()
     post_count = Post.objects.count()
     thread_count = Thread.objects.count()
-    return render(request, "main_page.html", {"user_count":user_count, "post_count":post_count, "thread_count": thread_count})
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    return render(request, "main_page.html", {"user_count":user_count, "post_count":post_count, "thread_count": thread_count, "num_visits": num_visits})
 
 
 def board(request, board):
