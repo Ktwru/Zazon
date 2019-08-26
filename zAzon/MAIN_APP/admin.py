1b77056e519192e2721cc6f569bcb4a4ac51ccef
Ktwru <spacewarlushd@gmail.com>
2019-08-26 15:14:36 +0300

    User-model
    deleted my user-model, imported django.auth-user-model

--- zAzon/MAIN_APP/admin.py
+++ zAzon/MAIN_APP/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, User_info, Thread, Post
+from .models import User_info, Thread, Post
 
-admin.site.register(User)
+
 admin.site.register(User_info)
 admin.site.register(Post)
 admin.site.register(Thread)
--- zAzon/MAIN_APP/models.py
+++ zAzon/MAIN_APP/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-class User(models.Model):
-    login = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.login
+from django.contrib.auth.models import User
 
 
 class Thread(models.Model):
