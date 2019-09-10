c6d3dd8a0a35941b2798c315af4d2ca49e1ab352
Ktwru <spacewarlushd@gmail.com>
2019-09-10 23:26:39 +0300

    styles are almost done

--- zAzon/MAIN_APP/views.py
+++ zAzon/MAIN_APP/views.py
@@ -100,15 +100,17 @@ def thread(request, board, thread_id):
                               "login": post.login,
                               "pic": post.pic,
                               "status": details.status,
-                              "user_pic": details.pic})
+                              "user_pic": details.pic,})
         thread = Thread.objects.get(id=thread_id)
         threadone = {"date": thread.date, "pic": thread.pic, "thread": thread.thread,
                      "login": thread.login, "op_post": thread.op_post,
-                     "op_pic": User_det.objects.get(username=thread.login).pic}
+                     "op_pic": User_det.objects.get(username=thread.login).pic,
+                     "count": Post.objects.filter(thread=thread).count(),
+                     "id": thread.id}
         if request.method == 'POST':
             post = request.POST.get('post')
             if 'pic' in request.FILES:
-                file = request.FILES['pic', False]
+                file = request.FILES['pic']
             else:
                 file = None
             login = User.objects.get(username=request.user.username)
